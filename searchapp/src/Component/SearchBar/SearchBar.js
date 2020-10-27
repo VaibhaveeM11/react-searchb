0043e723f44e0049f77a7cd8e116b95fd2e2fb5d
Vaibhavee Shinde <43693189+VaibhaveeM11@users.noreply.github.com>
2020-10-27 20:29:10 +0530

    Update SearchBar.js

--- searchapp/src/Component/SearchBar/SearchBar.js
+++ searchapp/src/Component/SearchBar/SearchBar.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './searchBar.css'
 function SearchBar()
 {
-return(
 
+
+    const[input,setInput]=useState('');
+    let nba=[
+        {name:'vaibhavee', team:'xyz'},
+        {name:'gajanan', team:'xyz'}, 
+        {name:'m', team:'xyz'}, 
+        {name:'john', team:'xyz'}
+
+]
+const handleChange=(e)=>{
+    e.preventDefault();
+    setInput(e.target.value);
+
+}
+if(nba.length>0)
+{
+    nba=nba.filter((i)=>{
+        return i.name.match(input);
+    })
+}
+return(
 <div className='search-card'>
     <form>
-             
-             <div>
-             <input type='text' id='search-input'/>
-<button type='button' id='search-button'>Search</button>
+     <div>
+             <input type='text' 
+             placeholder=' you can find here....?'
+                 onChange={handleChange}
+                 value={input}
+             />
+<button type='button' >Search</button>
 
              </div>
     
     </form>
+
+    <div>
+        {nba.map((fname,index)=>{
+            return (<div>
+
+                <ul>
+                    <li>{fname.name}+{fname.team}</li>
+                </ul>
+            </div>)
+        })}
+    </div>
 </div>)
 }
 export default SearchBar; 
